a5b8c969f7b93131306fe3f2f0a780eac7ed0ff4
Harish0916 <harish0916ojhacs@gmail.com>
2024-08-12 15:05:30 +0530

    Commit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.write("Welcome Harsh<br>")
-document.write("Welcome Harsh<br>")
-document.write("Welcome Harsh")
+// document.write("Welcome <br>")
+// let a = "3"
+// let b = 3
+// console.log(typeof(a));
+
+// let a = 2
+{
+    var a = 3
+}
+console.log(a);
 
